da3f857210cf98022bc4b6507e8b8538d81e4684
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-03-29 21:35:38 +0900

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
 
+const Item = ({ post }) => (
+  <div>
+    <Link className="has-text-primary" to={post.fields.slug}>
+      {post.frontmatter.title}
+    </Link>
+    <span> &bull; </span>
+    <small>{post.frontmatter.date}</small>
+    {post.excerpt}
+    <Link className="button is-small" to={post.fields.slug}>
+      Keep Reading →
+    </Link>
+  </div>
+);
 export default class IndexPage extends React.Component {
   render() {
-    console.log(this.props)
-    
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-          </div>
-          {posts
-            .filter(post => post.node.frontmatter.templateKey === 'blog-post')
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
-        </div>
+      <section>
+        {posts.map(({ node: post }) => <Item post={post} key={post.id} />)}
       </section>
-    )
+    );
   }
 }
 
@@ -56,7 +38,10 @@ export default class IndexPage extends React.Component {
 
 export const query = graphql`
   query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+    ) {
       edges {
         node {
           excerpt(pruneLength: 400)
@@ -73,7 +58,7 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 // export const query2 = graphql`
 //   query IndexQuery {
 //     allMarkdownRemark(sort: { fields: [frontmatter___date] }) {
