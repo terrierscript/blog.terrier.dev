1895cd47682f4d83a8034048516777541f4fa8a1
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-01 19:22:53 +0900

    tsc init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: "Gatsby + Netlify CMS Starter"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [],
-      },
+        plugins: []
+      }
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-  ],
-}
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
+    }
+  ]
+};
