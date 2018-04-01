1bfa44d712726a6163f83744442f6928d98eeb01
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-01 21:37:03 +0900

    add

--- src/templates/tags.tsx
+++ src/templates/tags.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
+import React from "react";
+import Helmet from "react-helmet";
+import Link from "gatsby-link";
 
-class TagRoute extends React.Component {
+class TagRoute extends React.Component<any, any> {
   render() {
-    const posts = this.props.data.allMarkdownRemark.edges
+    const posts = this.props.data.allMarkdownRemark.edges;
     const postLinks = posts.map(post => (
       <li key={post.node.fields.slug}>
         <Link to={post.node.fields.slug}>
           <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
         </Link>
       </li>
-    ))
-    const tag = this.props.pathContext.tag
-    const title = this.props.data.site.siteMetadata.title
-    const totalCount = this.props.data.allMarkdownRemark.totalCount
+    ));
+    const tag = this.props.pathContext.tag;
+    const title = this.props.data.site.siteMetadata.title;
+    const totalCount = this.props.data.allMarkdownRemark.totalCount;
     const tagHeader = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`
+      totalCount === 1 ? "" : "s"
+    } tagged with “${tag}”`;
 
     return (
       <section className="section">
@@ -26,7 +26,7 @@ class TagRoute extends React.Component {
           <div className="columns">
             <div
               className="column is-10 is-offset-1"
-              style={{ marginBottom: '6rem' }}
+              style={{ marginBottom: "6rem" }}
             >
               <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
               <ul className="taglist">{postLinks}</ul>
@@ -37,11 +37,11 @@ class TagRoute extends React.Component {
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
 
-export default TagRoute
+export default TagRoute;
 
 export const tagPageQuery = graphql`
   query TagPage($tag: String) {
@@ -68,4 +68,4 @@ export const tagPageQuery = graphql`
       }
     }
   }
-`
+`;
