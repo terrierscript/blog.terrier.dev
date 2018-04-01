83d366a87b5854c9371a530ff124671f3a994c25
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-01 19:02:36 +0900

    add

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import React from "react";
+import PropTypes from "prop-types";
+import { BlogPostTemplate } from "../../templates/blog-post";
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-)
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  return <div>aa</div>;
+  console.log(entry.toJson());
+  return (
+    <BlogPostTemplate
+      content={widgetFor("body")}
+      description={entry.getIn(["data", "description"])}
+      tags={entry.getIn(["data", "tags"])}
+      title={entry.getIn(["data", "title"])}
+    />
+  );
+};
 
-BlogPostPreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
-}
-
-export default BlogPostPreview
+export default BlogPostPreview;
