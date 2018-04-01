f848c0963b11a0968b1967606624f4e8215a6be0
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-01 19:26:43 +0900

    fix

--- src/templates/about-page.tsx
+++ src/templates/about-page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Content, { HTMLContent } from '../components/Content'
+import * as React from "react";
+// import PropTypes from 'prop-types'
+import Content, { HTMLContent } from "../components/Content";
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+  const PageContent = contentComponent || Content;
 
   return (
     <section className="section section--gradient">
@@ -20,17 +20,17 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+  contentComponent: PropTypes.func
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <AboutPageTemplate
@@ -38,14 +38,14 @@ const AboutPage = ({ data }) => {
       title={post.frontmatter.title}
       content={post.html}
     />
-  )
-}
+  );
+};
 
-AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+// AboutPage.propTypes = {
+//   data: PropTypes.object.isRequired,
+// }
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -56,4 +56,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
