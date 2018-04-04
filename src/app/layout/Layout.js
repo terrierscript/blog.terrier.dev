6d9ba7c3a0fc4dd575086257d54e7b21af6d8952
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-04 19:58:49 +0900

    add

--- src/app/layout/Layout.js
+++ src/app/layout/Layout.js
@@ -1,16 +1,16 @@
-import React, { SFC } from "react"
-import Navbar from "./Navbar"
-import styled from "styled-components"
+import React, { SFC } from "react";
+import Navbar from "./Navbar";
+import styled from "styled-components";
 // @ts-ignore
-import { desktop } from "../lib/media"
-import { Meta } from "./Meta"
-import { Container } from "./Container"
-import { Article } from "../Content"
-import ReactGA from "react-ga"
-import "prismjs/themes/prism-okaidia.css"
+import { desktop } from "../lib/media";
+import { Meta } from "./Meta";
+import { Container } from "./Container";
+import { Article } from "../Content";
+import ReactGA from "react-ga";
+// import "prismjs/themes/prism-okaidia.css"
 
-ReactGA.initialize("UA-5982830-12")
-ReactGA.pageview(window.location.pathname + window.location.search)
+ReactGA.initialize("UA-5982830-12");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export const Layout = ({ children }) => {
   return (
@@ -22,5 +22,5 @@ export const Layout = ({ children }) => {
         <Article>{children()}</Article>
       </Container>
     </div>
-  )
-}
+  );
+};
