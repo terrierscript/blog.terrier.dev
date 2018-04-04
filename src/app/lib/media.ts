9357801d409071ce2e8269d91418f21f85e73cc0
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-04 19:56:09 +0900

    add

--- src/app/lib/media.ts
+++ src/app/lib/media.ts
@@ -1,28 +1,28 @@
-import { css } from "styled-components"
+import { css } from "styled-components";
 
 const sizes = {
   desktop: 992,
   // tablet: 768,
   phone: 376
-}
+};
 
-type Types = keyof typeof sizes
+type Types = keyof typeof sizes;
 
 // TODO
 const media: any = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => {
     // @ts-ignore
-    const cssval = css(...args)
+    const cssval = css(...args);
     return css`
       @media (max-width: ${sizes[label] / 16}em) {
         ${cssval};
       }
-    `
-  }
-  return acc
-}, {})
+    `;
+  };
+  return acc;
+}, {});
 
 export default {
   desktop: media.desktop,
   phone: media.phone
-}
+};
