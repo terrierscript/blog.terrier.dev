64d3b1ce5b283e87cba3b4df923282f446cf3dae
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-04 10:21:17 +0900

    add

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BlogPostTemplate } from "../../templates/blog-post";
+import React from "react"
+import { BlogPostTemplate } from "../../templates/BlogPostTemplate"
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
   return (
     <BlogPostTemplate
       content={widgetFor("body")}
-      description={entry.getIn(["data", "description"])}
+      // description={entry.getIn(["data", "description"])}
       tags={entry.getIn(["data", "tags"])}
       title={entry.getIn(["data", "title"])}
     />
-  );
-};
+  )
+}
 
-export default BlogwPostPreview;
+export default BlogwPostPreview
