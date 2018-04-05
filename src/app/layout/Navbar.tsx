f547c3073f65895f68aece46ebb951643114f576
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-05 13:37:08 +0900

    add

--- src/app/layout/Navbar.tsx
+++ src/app/layout/Navbar.tsx
@@ -58,7 +58,7 @@ const NavbarContainer = ({ children }) => (
 const Navbar = () => (
   <div>
     <NavbarContainer>
-      <Logo to="/">Code Snippet</Logo>
+      <Logo to="/">Code Snippet 🐶</Logo>
     </NavbarContainer>
   </div>
 );
