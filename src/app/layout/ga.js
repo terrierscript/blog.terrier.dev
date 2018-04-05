0514c5e2253796478e22b758e571fcf58bea19b9
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-05 13:34:26 +0900

    debug flag

--- src/app/layout/ga.js
+++ src/app/layout/ga.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 
 export default () => {
-  ReactGA.initialize("UA-5982830-12");
+  ReactGA.initialize("UA-5982830-12", { debug: true });
   ReactGA.pageview(window.location.pathname + window.location.search);
   console.log("GA start");
 };
