f8f8423be2da7296508c102cba814bc116a15176
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-05 22:51:24 +0900

    breack

--- src/app/layout/Container.js
+++ src/app/layout/Container.js
@@ -1,17 +1,18 @@
-import React, { SFC } from "react"
-import styled from "styled-components"
+import React, { SFC } from "react";
+import styled from "styled-components";
 
 const Row = styled.div`
   display: flex;
-`
+`;
 const Col = styled.div`
   max-width: 740px;
   padding: 0 20px;
   margin: 0 auto;
-`
+  word-break: break-all;
+`;
 
 export const Container = ({ children }) => (
   <Row>
     <Col>{children}</Col>
   </Row>
-)
+);
