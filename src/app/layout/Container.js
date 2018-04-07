184712a181a05d513a0768c69d419dd4473dea97
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-07 16:51:18 +0900

    add

--- src/app/layout/Container.js
+++ src/app/layout/Container.js
@@ -1,17 +1,18 @@
-import React, { SFC } from "react"
-import styled from "styled-components"
+import React, { SFC } from "react";
+import styled from "styled-components";
+import { mobile } from "../lib/media";
 
 const Row = styled.div`
   display: flex;
-`
+`;
 const Col = styled.div`
   max-width: 740px;
   padding: 0 20px;
   margin: 0 auto;
-`
+`;
 
 export const Container = ({ children }) => (
   <Row>
     <Col>{children}</Col>
   </Row>
-)
+);
