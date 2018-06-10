e10cbbf5efd098840a5c12993fb452051b3eab71
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-06-10 17:12:47 +0900

    add

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -6,11 +6,9 @@ import { desktop } from "../lib/media"
 import { Meta } from "./Meta"
 import { Container } from "./Container"
 import { Article } from "../Content"
-import loadGA from "./ga.js"
 import { Footer } from "./Footer"
 
 export const Layout: SFC<{ children: any }> = ({ children }) => {
-  loadGA()
   return (
     <div>
       <Meta />
