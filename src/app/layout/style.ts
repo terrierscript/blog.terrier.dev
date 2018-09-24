657dfd4b07fd44ab563e8c4fb701ab773e545efb
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-09-24 10:55:02 +0900

    remvoe ts import

--- src/app/layout/style.ts
+++ src/app/layout/style.ts
@@ -1,10 +1,10 @@
-import { injectGlobal } from "styled-components";
-import normalize from "styled-normalize";
+// import { injectGlobal } from "styled-components";
+// import normalize from "styled-normalize";
 // @ts-ignore
 // import "prismjs/themes/prism-okaidia.css";
 // import "highlight.js/styles/dark.css";
 
-import { defaultFont } from "./font";
+// import { defaultFont } from "./font"
 
 // export const baseStyles = () => injectGlobal`
 // ${normalize}
