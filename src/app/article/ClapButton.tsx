841741293449e6b9af14719923a8e077a08ab1d3
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-16 21:06:23 +0900

    animating

--- src/app/article/ClapButton.tsx
+++ src/app/article/ClapButton.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useState, useRef } from "react"
+import React, { useCallback } from "react"
 import styled from "styled-components"
-import { Subject, queueScheduler } from "rxjs"
+import posed from "react-pose"
 
 const PositionFixed = styled.div`
   position: fixed;
@@ -8,40 +8,60 @@ const PositionFixed = styled.div`
   bottom: 50px;
 `
 const Button = styled.div`
-  border: #e2468a;
+  border: #e2468a 2px solid;
   border-radius: 50%;
   width: 2em;
   height: 2em;
   line-height: 2em;
   font-size: 2em;
   text-align: center;
+  background: white;
   vertical-align: middle;
   cursor: pointer;
+  transition: 0.3s;
+  user-select: none;
 `
 
 const url = "http://localhost:9000/clap"
 
 const useClapCallback = (title, id) => {
-  return useCallback(
-    () => {
-      return fetch(url, {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ id, title })
-      })
-    },
-    [title, id]
-  )
+  return useCallback(() => {
+    return fetch(url, {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ id, title })
+    })
+  }, [title, id])
 }
+
+const Item = posed.div({
+  pressable: true,
+  hoverable: true,
+  init: {
+    scale: 1
+  },
+  press: {
+    scale: 0.3
+  },
+  hover: {
+    scale: 1.2
+  }
+})
 export const ClapButton = ({ title, id, children }) => {
-  const onClick = useClapCallback(title, id)
+  const clapCallback = useClapCallback(title, id)
+  const onClick = useCallback(() => {
+    clapCallback()
+  }, [clapCallback])
+
   return (
     <PositionFixed>
-      <Button onClick={onClick}>{children}</Button>
+      <Item>
+        <Button onClick={onClick}>{children}</Button>
+      </Item>
     </PositionFixed>
   )
 }
