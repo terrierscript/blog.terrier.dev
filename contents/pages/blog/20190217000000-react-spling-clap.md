e6bb4d2bcb9983b59724f0ccc1dc743c2f74bd18
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-17 20:51:45 +0900

    add

---
+++ contents/pages/blog/20190217000000-react-spling-clap.md
@@ -0,0 +1,11 @@
+---
+templateKey: blog-post
+title: react-springで拍手ボタンにアニメーションをつけた
+date: 2019-02-17T20:45:04.240Z
+tags:
+  - React Spring
+  - Javascript
+  - React
+---
+
+GITHUB-EMBED
--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,17 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
+      resolve: 'gatsby-remark-github',
+      options: {
+        marker: 'GITHUB-EMBED',
+        insertEllipsisComments: true,
+        ellipsisPhrase: '...',
+        useCache: true,
+        cacheKey: 'gatsby-remark-github-v1',
+        token: process.env.GITHUB_TOKEN,
+      }
+    },
+    {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
