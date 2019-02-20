824bafd23308b30f323bf12298289ca4fa2cf43b
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-20 21:29:59 +0900

    add

--- scripts/create.js
+++ scripts/create.js
@@ -1,11 +1,27 @@
 const inquirer = require("inquirer")
-const slugify = require("slugify")
+const dashify = require("dashify")
 const { DateTime } = require("luxon")
-const matter = require("gray-matter")
+const grayMatter = require("gray-matter")
 const fs = require("fs")
 const exec = require("child_process").exec
 
 const now = DateTime.local()
+
+const convert = ({ title, slug, tags }) => {
+  const timestamp = now.toFormat("yyyyMMdd000000")
+  const dir = "contents/pages/blog"
+  const filename = `${dir}/${timestamp}-${slug}.md`
+  const matter = grayMatter.stringify("", {
+    templateKey: "blog-post",
+    title,
+    date: now.toUTC().toISO(),
+    tags: tags.split(" ")
+  })
+  return {
+    filename,
+    matter
+  }
+}
 inquirer
   .prompt([
     {
@@ -13,22 +29,29 @@ inquirer
     },
     {
       name: "slug",
-      default: ans => slugify(ans.title)
+      default: ({ title }) => dashify(title, { condense: true })
     },
     {
       name: "tags"
+    },
+    {
+      type: "confirm",
+      name: "confirm",
+      default: "Y",
+      message: answer => {
+        const { filename, matter } = convert(answer)
+        return [
+          "",
+          `Filename: ${filename}`,
+          "Matter:",
+          `${matter}`,
+          "OK?"
+        ].join("\n")
+      }
     }
   ])
-  .then(({ title, slug, tags }) => {
-    const timestamp = now.toFormat("yyyyMMdd000000")
-    const dir = "contents/pages/blog"
-    const filename = `${dir}/${timestamp}-${slug}.mdx`
-    const mt = matter.stringify("", {
-      templateKey: "blog-post",
-      title,
-      date: now.toUTC().toISO(),
-      tags: tags.split(" ")
-    })
-    fs.writeFileSync(filename, mt)
-    exec(`code ${fileame}`)
+  .then(answer => {
+    const { filename, matter } = convert(answer)
+    fs.writeFileSync(filename, matter)
+    exec(`code ${filename}`)
   })
