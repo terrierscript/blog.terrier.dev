a592a5ac52a8cb088dd84630724d47bbdbf2de4d
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-03-05 00:57:39 +0900

    speed?

--- app/article/toHtmlAst.ts
+++ app/article/toHtmlAst.ts
@@ -8,22 +8,22 @@ const renderAst = new rehypeReact({
 }).Compiler
 
 const nr2br = htmlAst => {
-  return map(htmlAst, (node, i, parent) => {
+  return map(htmlAst, (node, index, parent) => {
     if (node.type !== "text") return node
     if (parent.tagName !== "p") return node
     const value = node.value.trim()
     if (value.indexOf("\n") < 0) {
       return node
     }
-    const children = value
-      .split("\n")
-      .map((v, i) => {
-        return i == 0
-          ? { type: "text", value: v }
-          : [{ type: "element", tagName: "br" }, { type: "text", value: v }]
-      })
-      .reduce((a, b) => a.concat(b), []) // TODO: flatten
-
+    const c = value.split("\n").map((v, i) => {
+      return i == 0
+        ? [{ type: "text", value: v }]
+        : [{ type: "element", tagName: "br" }, { type: "text", value: v }]
+    })
+    let children = []
+    for (let i = 0; i < c.length; i++) {
+      children = [...children, ...c[i]]
+    }
     // TODO: inject to parent
     return {
       type: "element",
