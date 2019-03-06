b9ee50706ab38cd17e136986d7daae1d3887b6f4
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-03-06 23:09:53 +0900

    paginate

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,10 +1,30 @@
 import React from "react"
 // import PropTypes from "prop-types";
 import { BlogItem, BlogList } from "../../app/list/Item"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { generatePostFragment } from "../query/query"
 import { BlogLayout } from "../provider/BlogLayout"
+import styled from "styled-components"
+import { headerFont } from "../../app/utils/typography"
 
+const PaginateContainer = styled.div`
+  padding: 1em;
+  justify-content: space-evenly;
+  width: 100%;
+  display: flex;
+  font-weight: bold;
+  font-size: 1.2em;
+  font-family: ${headerFont};
+`
+
+const Paginate = ({ previousPagePath, nextPagePath }) => {
+  return (
+    <PaginateContainer>
+      <Link to={previousPagePath}>≪Prev</Link>
+      <Link to={nextPagePath}>Next≫</Link>
+    </PaginateContainer>
+  )
+}
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
@@ -13,6 +33,7 @@ export default class IndexPage extends React.Component {
     return (
       <BlogLayout>
         <BlogList posts={posts} />
+        <Paginate {...this.props.pathContext} />
       </BlogLayout>
     )
   }
@@ -20,7 +41,7 @@ export default class IndexPage extends React.Component {
 
 export const postFragment = generatePostFragment()
 export const query = graphql`
-  query IndexQuery {
+  query IndexQuery($skip: Int!, $limit: Int!) {
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] }
       filter: {
@@ -29,6 +50,8 @@ export const query = graphql`
           published: { ne: false }
         }
       }
+      skip: $skip
+      limit: $limit
     ) {
       edges {
         node {
