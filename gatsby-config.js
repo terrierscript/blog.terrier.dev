c392d537ea0e16e229b2a886fccba246a1549dfc
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-03-10 15:33:09 +0900

    remove admin

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,12 +46,6 @@ module.exports = {
         ]
       }
     },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
-      }
-    },
     "gatsby-plugin-twitter",
     "gatsby-plugin-feed",
     "gatsby-plugin-styled-components",
--- src/cms/cms.js
+++
@@ -1,8 +0,0 @@
-import CMS from "netlify-cms"
-
-// import AboutPagePreview from "./preview-templates/AboutPagePreview";
-import BlogPostPreview from "./preview-templates/BlogPostPreview"
-// import ProductPagePreview from './preview-templates/ProductPagePreview'
-
-// CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate("blog", BlogPostPreview)
