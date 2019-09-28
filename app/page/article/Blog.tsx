28fd0a2918cd0dc86c8f80b9dc40d016cf2f7d12
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-28 13:27:13 +0900

    wip

--- app/page/article/Blog.tsx
+++ app/page/article/Blog.tsx
@@ -112,7 +112,24 @@ export const BlogPostTemplate = ({
         <BlogArticle title={title} content={content} tags={tags} date={date} />
         <Modify fileAbsolutePath={fileAbsolutePath} />
         {/* <Comment identifier={id} title={title} /> */}
+        <Adsense/>
       </BlogBody>
     </Layout>
   )
 }
+
+
+const Adsense = () => (
+  <>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-9836926490768601"
+     data-ad-slot="2319260593"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</>
+)
