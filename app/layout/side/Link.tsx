94d3d9171450810411ba927a25baa11a5bc0887a
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-05 21:19:49 +0900

    add note

--- app/layout/side/Link.tsx
+++ app/layout/side/Link.tsx
@@ -13,12 +13,13 @@ const Links = () => {
   return (
     <Ul>
       <List href="https://github.com/terrierscript">GitHub</List>
-      <List href="https://www.npmjs.com/~inuscript">npm</List>
       <List href="https://twitter.com/terrierscript">Twitter</List>
+      <List href="https://note.mu/terrierscript/">note</List>
+      <List href="https://scrapbox.io/terrierscript/">Scrapbox</List>
+      <List href="https://www.npmjs.com/~inuscript">npm</List>
       <List href="https://qiita.com/terrierscript">Qiita</List>
       <List href="https://medium.com/inuscript/">Medium</List>
       <List href="https://dev.to/terrierscript">dev.to</List>
-      <List href="https://scrapbox.io/terrierscript/">Scrapbox</List>
     </Ul>
   )
 }
