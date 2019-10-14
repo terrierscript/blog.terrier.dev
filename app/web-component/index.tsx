a201eab90d0dcac596b69e43399898f98a8767e6
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-14 19:22:01 +0900

    async

--- app/web-component/index.tsx
+++ app/web-component/index.tsx
@@ -1,7 +1,10 @@
 if (typeof window !== `undefined`) {
-  const { StackbritzIframe } = require("./Stackblitz")
-  const { SampleTimer } = require("./SampleTimer")
+  async function load() {
+    const { StackbritzIframe } = await import("./Stackblitz")
+    const { SampleTimer } = await import("./SampleTimer")
 
-  customElements.define("stackblitz-iframe", StackbritzIframe)
-  customElements.define("sample-timer", SampleTimer)
+    customElements.define("stackblitz-iframe", StackbritzIframe)
+    customElements.define("sample-timer", SampleTimer)
+  }
+  load()
 }
