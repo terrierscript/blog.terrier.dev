b83bf9beb02a7d251dfe2d4113b8980c8dbc73ff
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-19 19:36:12 +0900

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
 import React, { Component, FC } from "react"
-class Index extends Component {
-  render() {
-    console.log("idx")
-    return <div>Hello world!</div>
-  }
-}
+
 export default function() {
-  return <div>aa</div>
+  return <div>Hello world!</div>
 }
