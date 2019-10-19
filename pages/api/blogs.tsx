76b980bb4f3b0ad617206cdec25395815c2dcfa7
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-20 01:35:46 +0900

    add

--- pages/api/blogs.tsx
+++ pages/api/blogs.tsx
@@ -1,10 +1,15 @@
-import { getBlogList } from "../../src/markdown/module"
+// import { getBlogList } from "../../src/markdown/module"
 import { NextApiRequest, NextApiResponse } from "next"
+import { getPagenateList } from "../../src/markdown/files"
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  getBlogList(req.query).then(result => {
-    res.setHeader("Content-Type", "application/json")
-    res.statusCode = 200
-    res.end(JSON.stringify(result))
-  })
+  const result = getPagenateList(
+    Number(req.query.page),
+    Number(req.query.limit)
+  )
+  // getBlogList(req.query).then(result => {
+  res.setHeader("Content-Type", "application/json")
+  res.statusCode = 200
+  res.end(JSON.stringify(result))
+  // })
 }
