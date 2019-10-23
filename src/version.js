113233e531e95620699342426dbd1ffe3643c3c0
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-23 17:27:37 +0900

    add version

--- src/version.js
+++ src/version.js
@@ -1,6 +1,19 @@
-export const getVersion = () => {
-  return process.env.NOW_GITHUB_COMMIT_SHA || ""
+const getType = () => {
+  if (process.env.NOW_REGION) {
+    return "now.sh"
+  }
+  if (process.env.NETLIFY) {
+    return "netlify"
+  }
+  return "unknown"
 }
+export const getVersion = () => {
+  return JSON.stringify({
+    sha1: process.env.NOW_GITHUB_COMMIT_SHA || process.env.COMMIT_REF || "",
+    type: getType()
+  })
+}
+
 export const logVersion = () => {
   console.log(`Version: ${getVersion()}`)
 }
