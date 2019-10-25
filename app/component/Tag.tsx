17c3a20e215ecaa4ada11397615f0817de5cca25
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-25 23:06:03 +0900

    add

--- app/component/Tag.tsx
+++ app/component/Tag.tsx
@@ -5,15 +5,15 @@ import { headerFont } from "../utils/typography"
 
 export const TagItem = styled.div`
   display: inline-block;
-  /* background: #e08e79;
-  color: #000;
+  background: #e08e79;
   font-family: ${headerFont};
+  color: #000;
   font-weight: bold;
   padding: 0.2em 0.5em 0.3em;
   margin: 0.2em;
   border-radius: 20px;
   font-size: 0.8em;
-  text-transform: lowercase; */
+  text-transform: lowercase;
   ::before {
     content: "# ";
   }
