ee7a37990c1f0a3017b866188ecfe3625ea25353
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-25 23:09:22 +0900

    add font

--- app/utils/typography.js
+++ app/utils/typography.js
@@ -1,28 +1,28 @@
 import Typography from "typography"
 // import fairyGatesTheme from "typography-theme-fairy-gates";
 
-// export const headerFont = "Quicksand"
-export const headerFont = "Hiragino Kaku Gothic"
+export const headerFont = "Quicksand"
+// export const headerFont = "Hiragino Kaku Gothic"
 
 const theme = {
   baseFontSize: "18px",
-  // googleFonts: [
-  //   {
-  //     name: "Quicksand",
-  //     styles: ["400", "500", "700"]
-  //   },
-  //   {
-  //     name: "Nunito",
-  //     styles: ["400", "600", "700", "800", "900"]
-  //     // styles: [400, 500]
-  //   },
-  //   {
-  //     // hack for SSR:
-  //     // https://github.com/KyleAMathews/typography.js/issues/211#issuecomment-494001270
-  //     name: "Roboto Condensed",
-  //     styles: ["700&display=optional"]
-  //   }
-  // ],
+  googleFonts: [
+    {
+      name: "Quicksand",
+      styles: ["400", "500", "700"]
+    },
+    {
+      name: "Nunito",
+      styles: ["400", "600", "700", "800", "900"]
+      // styles: [400, 500]
+    },
+    {
+      // hack for SSR:
+      // https://github.com/KyleAMathews/typography.js/issues/211#issuecomment-494001270
+      name: "Roboto Condensed",
+      styles: ["700&display=optional"]
+    }
+  ],
   headerFontFamily: [headerFont, "Helvetica"],
   bodyFontFamily: [
     "Hiragino Kaku Gothic",
