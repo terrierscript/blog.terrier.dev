28bb48b5d93874027fa4e785591fffcc941c14df
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-26 15:07:09 +0900

    code 2em

--- app/page/article/Markdown.tsx
+++ app/page/article/Markdown.tsx
@@ -3,12 +3,19 @@ import ReactMarkdown from "react-markdown/with-html" // for web components
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import style from "react-syntax-highlighter/dist/cjs/styles/prism/tomorrow"
 import { nl2brRemark } from "./unified/nl2br"
+import styled from "@emotion/styled"
+
+const CodeWrapper = styled.div`
+  margin-bottom: 2rem;
+`
 
 export const CodeBlock = ({ value, language }) => {
   return (
-    <SyntaxHighlighter language={language} style={style}>
-      {value}
-    </SyntaxHighlighter>
+    <CodeWrapper>
+      <SyntaxHighlighter language={language} style={style}>
+        {value}
+      </SyntaxHighlighter>
+    </CodeWrapper>
   )
 }
 
