d0a312cd5fc735424460706e5f9440f1834705a5
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-26 12:35:51 +0900

    add

--- app/page/article/unified/nl2br.ts
+++ app/page/article/unified/nl2br.ts
@@ -1,6 +1,7 @@
 import visit, { Visitor } from "unist-util-visit"
 import { Node, Parent } from "unist"
 
+// core
 const injectChild = (parent: Parent, children: Node[], index) => {
   return [
     ...parent.children.slice(0, index),
@@ -13,26 +14,44 @@ const nodeLines = node => node.value.trim().split("\n")
 const isParent = (node: Node): node is Parent =>
   typeof node.children !== "undefined" && Array.isArray(node.children)
 
-export const nl2brRemark = () => {
-  const visitor: Visitor<Node> = (node: Node, index, parent: Node) => {
-    if (node.type !== "text") return
-    if (!isParent(parent)) return
-
-    const lines = nodeLines(node)
-    if (lines < 2) {
-      return
-    }
-
-    const children = lines
+const travarseLine = (valueToNode: (v) => Node, brNode: Node) => {
+  return (lines: string[]) =>
+    lines
       .map((v, i) => {
-        return i == 0
-          ? [{ type: "text", value: v }]
-          : [{ type: "break" }, { type: "text", value: v }]
+        return i == 0 ? [valueToNode(v)] : [brNode, valueToNode(v)]
       })
-      .reduce((a, b) => [...a, ...b], [])
-    const newChildren = injectChild(parent, children, index)
-    parent.children = newChildren
+      .reduce((a, b) => [...a, ...b], []) // TODO: .flat()
+}
+
+const nl2brVisitor = (isTargetNode, lineTravarse): Visitor<Node> => (
+  node: Node,
+  index,
+  parent: Node
+) => {
+  if (!isTargetNode(node, parent)) return
+  if (!isParent(parent)) return
+
+  const lines = nodeLines(node)
+  if (lines < 2) {
+    return
   }
+  const children = lineTravarse(lines)
+  const newChildren = injectChild(parent, children, index)
+  parent.children = newChildren
+}
+
+const remarkLineTraverse = travarseLine(v => ({ type: "text", value: v }), {
+  type: "break"
+})
+
+const isValidRemarkNode = (node: Node, parent: Node) => {
+  return node.type === "text"
+}
+
+// remark
+
+export const nl2brRemark = () => {
+  const visitor = nl2brVisitor(isValidRemarkNode, remarkLineTraverse)
   const transformer = tree => {
     visit(tree, visitor)
     return tree
@@ -40,27 +59,18 @@ export const nl2brRemark = () => {
   return transformer
 }
 
-export const nl2brRehype = () => {
-  const visitor: Visitor<Node> = (node: Node, index, parent: Node) => {
-    if (node.type !== "text") return
-    if (parent.tagName !== "p") return
-    if (!isParent(parent)) return
+// rehype
+const rehypeLineTraverse = travarseLine(v => ({ type: "text", value: v }), {
+  type: "element",
+  tagName: "br"
+})
 
-    const lines = nodeLines(node)
-    if (lines < 2) {
-      return
-    }
-    const children = lines
-      .map((v, i) => {
-        return i == 0
-          ? [{ type: "text", value: v }]
-          : [{ type: "element", tagName: "br" }, { type: "text", value: v }]
-      })
-      // .flat()
-      .reduce((a, b) => [...a, ...b], []) // TODO: Array.prototype.flat
-    const newChildren = injectChild(parent, children, index)
-    parent.children = newChildren
-  }
+const isValidRehypeNode = (node: Node, parent: Node) => {
+  return node.type === "text" && parent.tagName === "p"
+}
+
+export const nl2brRehype = () => {
+  const visitor = nl2brVisitor(isValidRehypeNode, rehypeLineTraverse)
 
   const transformer = tree => {
     visit(tree, visitor)
