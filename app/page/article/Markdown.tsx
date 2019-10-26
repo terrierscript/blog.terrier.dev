d46c16ef68edd06555682492f75e35b21724409c
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-26 12:53:01 +0900

    add

--- app/page/article/Markdown.tsx
+++ app/page/article/Markdown.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react"
 import ReactMarkdown from "react-markdown"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import style from "react-syntax-highlighter/dist/cjs/styles/prism/tomorrow"
-import { nl2brRemark } from "./unified/nl2brRemark"
+import { nl2brRemark } from "./unified/nl2br"
 
 export const CodeBlock = ({ value, language }) => {
   return (
