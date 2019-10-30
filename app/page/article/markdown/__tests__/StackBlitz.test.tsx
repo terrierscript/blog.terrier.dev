3cb05c0a91b66daed550f01138f103b63429994b
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-10-30 15:29:45 +0900

    append

--- app/page/article/markdown/__tests__/StackBlitz.test.tsx
+++ app/page/article/markdown/__tests__/StackBlitz.test.tsx
@@ -4,8 +4,14 @@ import { RawMarkdown } from "../Markdown"
 
 describe("StackBlitz iframe", () => {
   it("sample", () => {
+    const example = "https://stackblitz.com/edit/react-ts-egg6dg"
+    const tree = renderer.create(<RawMarkdown markdown={example} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it("sample with params", () => {
     const example =
       "https://stackblitz.com/edit/react-ts-egg6dg?embed=1&file=index.tsx&hideExplorer=1&view=preview&ctl=1"
     const tree = renderer.create(<RawMarkdown markdown={example} />).toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
