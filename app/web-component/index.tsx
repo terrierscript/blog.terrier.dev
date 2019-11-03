d9ace5295d9d1aafe3fdc61c986794df1da5012a
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-11-03 22:15:58 +0900

    remove

--- app/web-component/index.tsx
+++ app/web-component/index.tsx
@@ -1,8 +1,5 @@
 if (typeof window !== `undefined`) {
-  const { StackbritzIframe } = require("./Stackblitz")
   const { SampleTimer } = require("./SampleTimer")
 
-  // customElements.define("stackblitz-iframe", StackbritzIframe)
-  customElements.define("sb-iframe", StackbritzIframe)
   customElements.define("sample-timer", SampleTimer)
 }
