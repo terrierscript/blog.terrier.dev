7a0edb67b6e6ab38b4c6f62bf2037ca8fa904c30
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-11-09 13:13:08 +0900

    fix dummy

--- app/lib/feed/fromDummy.js
+++ app/lib/feed/fromDummy.js
@@ -1,6 +1,13 @@
 const { from } = require("rxjs")
 const { map } = require("rxjs/operators")
-const { generateMock } = require("./loader")
+
+const generateMock = () => ({
+  title: "Mock",
+  link: `mock${Math.random()}`,
+  dummy: true,
+  date: new Date()
+})
+
 const fromDummy = config => {
   return from(Array(2).fill(null)).pipe(
     map(_ => ({
