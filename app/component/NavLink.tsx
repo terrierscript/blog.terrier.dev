754868c087700442851f348708c6e5b3ff814d87
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-12-04 23:33:17 +0900

    add nav

--- app/component/NavLink.tsx
+++ app/component/NavLink.tsx
@@ -1,5 +1,7 @@
 // see: https://github.com/gatsbyjs/gatsby/issues/10668
 // import { Link } from "gatsby"
 import { default as GatsbyLink } from "gatsby-link"
+import { Link } from "@chakra-ui/core"
+import React from "react"
 
-export const NavLink = GatsbyLink
+export const NavLink = props => <Link as={GatsbyLink} {...props} />
