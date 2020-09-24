51a01324b664988b38b006a313b76605ace7e594
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-09-24 21:17:09 +0900

    add

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react"
 
-const NotFoundPage = () => (
-  <div>
+const NotFoundPage = () => {
+  const [maybe, setMaybe] = useState()
+  if (typeof window !== 'undefined') {
+    const href = window.location.href
+    const replaced = href.replace("/blog/", "/archive/")
+    if (replaced !== href) {
+      // @ts-ignore
+      setMaybe(replaced)
+    }
+  }
+
+  return <div>
     <h1>404</h1>
     <p>見つかりませんでした</p>
+    {maybe && <div><a href={maybe}>{maybe}</a>にあるかもしれません
+    </div>}
   </div>
-);
+}
 
-export default NotFoundPage;
+export default NotFoundPage
